484144d76efe5d5ff70e2521831ec54e00f1814d
Chaitanya Kumar <chaitanyak28771@gmail.com>
2022-12-11 13:45:24 +0100

    Solved idk like 44 simple exercises in one sitting

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,15 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(napples: usize) -> usize {
+    if napples <= 40 {
+        napples * 2
+    } else {
+        napples
+    }
+}
 
 // Don't modify this function!
 #[test]
